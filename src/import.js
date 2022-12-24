a9c59eaa8eec95012088b954bbccd38f5cb37740
Semenov <semenov@mail.ru>
2022-12-24 21:47:09 +0400

    настройка либы

--- src/import.js
+++ src/import.js
@@ -1,14 +1,9 @@
-import components from'./components.js'
+import ImagesBase from './components/imagesBase/imagesBase.vue';
+import InputBase from './components/formElement/inputBase/inputBase.vue';
+import CheckboxBase from './components/formElement/checkboxBase/checkboxBase.vue';
+import LabelBase from './components/formElement/labelBase/labelBase.vue';
+import SelectBase from './components/formElement/selectBase/selectBase.vue';
+import TagBase from './components/formElement/tagBase/tagBase.vue';
 
-const plugin = {
-  install (Vue) {
-    for (const prop in components) {
-      if (components.hasOwnProperty(prop)) {
-        const component = components[prop]
-        Vue.component(component.name, component)
-      }
-    }
-  }
-}
 
-export default plugin
+export { ImagesBase, InputBase, CheckboxBase, LabelBase, SelectBase, TagBase };
